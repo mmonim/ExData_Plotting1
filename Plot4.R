b2b5727563bd0ceedf442d584cff07ebb54ba15d
mmonim <mam_moneim@yahoo.com>
2016-04-16 23:06:55 +0300

    Added files via upload

---
+++ Plot4.R
@@ -0,0 +1,62 @@
+## The purpose of this project is to demonstrate ability to plot exploratory analysis
+##  This is plot4.R for the fourth plot
+
+setwd("Coursera/Exploratory Data Analysis/WEEK1")
+filename <- "exdata-data-household_power_consumption.zip"
+
+## check if file exists or downloaded before
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileURL, filename)
+}  
+
+## uncompress the file into current folder
+if (!file.exists("household_power_consumption.txt")) { 
+  unzip(filename) 
+}
+
+## - 'household_power_consumption.txt': 
+##
+## Date: Date in format dd/mm/yyyy
+## Time: time in format hh:mm:ss
+## Global_active_power: household global minute-averaged active power (in kilowatt)
+## Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+## Voltage: minute-averaged voltage (in volt)
+## Global_intensity: household global minute-averaged current intensity (in ampere)
+## Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+## Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+## Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+##
+
+DATA <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+DATA$Date <- as.Date(DATA$Date, format="%d/%m/%Y")
+
+## SELECT data for specific dates
+DATA <- subset(DATA, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+
+## coerce date time to POSIXct standard
+datetime <- paste(as.Date(DATA$Date), DATA$Time)
+DATA$Datetime <- as.POSIXct(datetime)
+
+## start plotting - Plot 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(data, {
+  plot(Global_active_power~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~Datetime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
+
+## Saving to PNG file
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
+
